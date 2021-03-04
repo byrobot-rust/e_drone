ff0df19db500082bcf8808c0c68524b8210f13a3
Sanghyo Lee <realwolf@naver.com>
2021-03-04 15:01:04 +0900

    add request

--- src/communication/transfer.rs
+++ src/communication/transfer.rs
@@ -3,6 +3,32 @@ use crate::base::system::DeviceType;
 use crate::communication::crc16;
 
 
+pub fn request(data_type: DataType) -> Vec<u8>
+{
+    let mut vec_data: Vec<u8> = Vec::new();
+
+    // start code
+    vec_data.push(0x0A);
+    vec_data.push(0x55);
+
+    // header
+    vec_data.push(DataType::Request.into());
+    vec_data.push(1);
+    vec_data.push(DeviceType::Base.into());
+    vec_data.push(DeviceType::Drone.into());
+
+    // data
+    vec_data.push(data_type.into());
+
+    // crc16
+    let crc: u16 = crc16::calc_array(0, &vec_data[2..]);
+    vec_data.push((crc & 0xFF) as u8);
+    vec_data.push((crc >> 8) as u8);
+
+    vec_data
+}
+
+
 pub fn control(roll: i8, pitch: i8, yaw: i8, throttle: i8) -> Vec<u8>
 {
     let mut vec_data: Vec<u8> = Vec::new();
@@ -23,8 +49,8 @@ pub fn control(roll: i8, pitch: i8, yaw: i8, throttle: i8) -> Vec<u8>
     vec_data.push(yaw as u8);
     vec_data.push(throttle as u8);
 
-    let crc: u16 = crc16::calc_array(0, &vec_data[..]);
-
+    // crc16
+    let crc: u16 = crc16::calc_array(0, &vec_data[2..]);
     vec_data.push((crc & 0xFF) as u8);
     vec_data.push((crc >> 8) as u8);
 
