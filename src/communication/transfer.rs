2c1c5587a05e289fcbef9581e7813a1533ad0594
Sanghyo Lee <realwolf@naver.com>
2021-03-08 15:39:55 +0900

    transfer에서 start code, crc16을 별도의 함수로 분리함

--- src/communication/transfer.rs
+++ src/communication/transfer.rs
@@ -3,13 +3,30 @@ use crate::base::system::DeviceType;
 use crate::communication::crc16;
 
 
-pub fn request(data_type: DataType) -> Vec<u8>
+pub fn start() -> Vec<u8>
 {
     let mut vec_data: Vec<u8> = Vec::new();
 
     // start code
     vec_data.push(0x0A);
     vec_data.push(0x55);
+
+    vec_data
+}
+
+
+pub fn add_crc16(vec_data: &mut Vec<u8>)
+{
+    // crc16
+    let crc: u16 = crc16::calc_array(0, &vec_data[2..]);
+    vec_data.push((crc & 0xFF) as u8);
+    vec_data.push((crc >> 8) as u8);
+}
+
+
+pub fn request(data_type: DataType) -> Vec<u8>
+{
+    let mut vec_data: Vec<u8> = start();
 
     // header
     vec_data.push(DataType::Request.into());
@@ -21,9 +38,7 @@ pub fn request(data_type: DataType) -> Vec<u8>
     vec_data.push(data_type.into());
 
     // crc16
-    let crc: u16 = crc16::calc_array(0, &vec_data[2..]);
-    vec_data.push((crc & 0xFF) as u8);
-    vec_data.push((crc >> 8) as u8);
+    add_crc16(&mut vec_data);
 
     vec_data
 }
@@ -31,11 +46,7 @@ pub fn request(data_type: DataType) -> Vec<u8>
 
 pub fn control(roll: i8, pitch: i8, yaw: i8, throttle: i8) -> Vec<u8>
 {
-    let mut vec_data: Vec<u8> = Vec::new();
-
-    // start code
-    vec_data.push(0x0A);
-    vec_data.push(0x55);
+    let mut vec_data: Vec<u8> = start();
 
     // header
     vec_data.push(DataType::Control.into());
@@ -50,9 +61,7 @@ pub fn control(roll: i8, pitch: i8, yaw: i8, throttle: i8) -> Vec<u8>
     vec_data.push(throttle as u8);
 
     // crc16
-    let crc: u16 = crc16::calc_array(0, &vec_data[2..]);
-    vec_data.push((crc & 0xFF) as u8);
-    vec_data.push((crc >> 8) as u8);
+    add_crc16(&mut vec_data);
 
     vec_data
 }
