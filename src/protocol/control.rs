84df59526cebb889f195687c2a5dce6acf247d25
Sanghyo Lee <realwolf@naver.com>
2021-03-18 15:44:30 +0900

    sensor, control 정의 추가

--- src/protocol/control.rs
+++ src/protocol/control.rs
@@ -1,4 +1,4 @@
-use crate::protocol::Serializable;
+use crate::protocol::{*};
 use crate::communication::extractor::Extractor;
 
 
@@ -52,4 +52,182 @@ impl Serializable for Quad8 {
         vec_data
     }
 }
+
+
+// -- Quad8AndRequestData -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Quad8AndRequestData {
+    pub roll: i8,
+    pub pitch: i8,
+    pub yaw: i8,
+    pub throttle: i8,
+    pub data_type: DataType,
+}
+
+
+impl Quad8AndRequestData {
+    pub fn new() -> Quad8AndRequestData{
+        Quad8AndRequestData {
+            roll: 0,
+            pitch: 0,
+            yaw: 0,
+            throttle: 0,
+            data_type: DataType::None,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Quad8AndRequestData, &'static str> {
+        if slice_data.len() == Quad8AndRequestData::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Quad8AndRequestData{
+                roll: ext.get_i8(),
+                pitch: ext.get_i8(),
+                yaw: ext.get_i8(),
+                throttle: ext.get_i8(),
+                data_type: DataType::from_u8(ext.get_u8()),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Quad8AndRequestData {
+    fn size() -> usize { 5 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.roll.to_le_bytes());
+        vec_data.extend_from_slice(&self.pitch.to_le_bytes());
+        vec_data.extend_from_slice(&self.yaw.to_le_bytes());
+        vec_data.extend_from_slice(&self.throttle.to_le_bytes());
+        vec_data.push(self.data_type.into());
+
+        vec_data
+    }
+}
+
+
+// -- Position16 -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Position16 {
+    pub position_x: i16,
+    pub position_y: i16,
+    pub position_z: i16,
+    pub velocity: i16,
+    pub heading: i16,
+    pub rotational_velocity: i16,
+}
+
+
+impl Position16 {
+    pub fn new() -> Position16{
+        Position16 {
+            position_x: 0,
+            position_y: 0,
+            position_z: 0,
+            velocity: 0,
+            heading: 0,
+            rotational_velocity: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Position16, &'static str> {
+        if slice_data.len() == Position16::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Position16{
+                position_x: ext.get_i16(),
+                position_y: ext.get_i16(),
+                position_z: ext.get_i16(),
+                velocity: ext.get_i16(),
+                heading: ext.get_i16(),
+                rotational_velocity: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Position16 {
+    fn size() -> usize { 12 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.position_x.to_le_bytes());
+        vec_data.extend_from_slice(&self.position_y.to_le_bytes());
+        vec_data.extend_from_slice(&self.position_z.to_le_bytes());
+        vec_data.extend_from_slice(&self.velocity.to_le_bytes());
+        vec_data.extend_from_slice(&self.heading.to_le_bytes());
+        vec_data.extend_from_slice(&self.rotational_velocity.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- Position -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Position {
+    pub position_x: f32,
+    pub position_y: f32,
+    pub position_z: f32,
+    pub velocity: f32,
+    pub heading: i16,
+    pub rotational_velocity: i16,
+}
+
+
+impl Position {
+    pub fn new() -> Position{
+        Position {
+            position_x: 0.0_f32,
+            position_y: 0.0_f32,
+            position_z: 0.0_f32,
+            velocity: 0.0_f32,
+            heading: 0,
+            rotational_velocity: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Position, &'static str> {
+        if slice_data.len() == Position::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Position{
+                position_x: ext.get_f32(),
+                position_y: ext.get_f32(),
+                position_z: ext.get_f32(),
+                velocity: ext.get_f32(),
+                heading: ext.get_i16(),
+                rotational_velocity: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Position {
+    fn size() -> usize { 20 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.position_x.to_le_bytes());
+        vec_data.extend_from_slice(&self.position_y.to_le_bytes());
+        vec_data.extend_from_slice(&self.position_z.to_le_bytes());
+        vec_data.extend_from_slice(&self.velocity.to_le_bytes());
+        vec_data.extend_from_slice(&self.heading.to_le_bytes());
+        vec_data.extend_from_slice(&self.rotational_velocity.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
 
--- src/protocol/sensor.rs
+++ src/protocol/sensor.rs
@@ -2,6 +2,161 @@ use crate::protocol::Serializable;
 use crate::communication::extractor::Extractor;
 
 
+// -- RawMotion -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct RawMotion {
+    pub accel_x: i16,
+    pub accel_y: i16,
+    pub accel_z: i16,
+    pub gyro_roll: i16,
+    pub gyro_pitch: i16,
+    pub gyro_yaw: i16,
+}
+
+
+impl RawMotion {
+    pub fn new() -> RawMotion{
+        RawMotion {
+            accel_x: 0,
+            accel_y: 0,
+            accel_z: 0,
+            gyro_roll: 0,
+            gyro_pitch: 0,
+            gyro_yaw: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<RawMotion, &'static str> {
+        if slice_data.len() == RawMotion::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(RawMotion{
+                accel_x: ext.get_i16(),
+                accel_y: ext.get_i16(),
+                accel_z: ext.get_i16(),
+        
+                gyro_roll: ext.get_i16(),
+                gyro_pitch: ext.get_i16(),
+                gyro_yaw: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for RawMotion {
+    fn size() -> usize { 12 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.accel_x.to_le_bytes());
+        vec_data.extend_from_slice(&self.accel_y.to_le_bytes());
+        vec_data.extend_from_slice(&self.accel_z.to_le_bytes());
+
+        vec_data.extend_from_slice(&self.gyro_roll.to_le_bytes());
+        vec_data.extend_from_slice(&self.gyro_pitch.to_le_bytes());
+        vec_data.extend_from_slice(&self.gyro_yaw.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- RawFlow -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct RawFlow {
+    pub x: f32,
+    pub y: f32,
+}
+
+
+impl RawFlow {
+    pub fn new() -> RawFlow{
+        RawFlow {
+            x: 0.0_f32,
+            y: 0.0_f32,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<RawFlow, &'static str> {
+        if slice_data.len() == RawFlow::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(RawFlow{
+                x: ext.get_f32(),
+                y: ext.get_f32(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for RawFlow {
+    fn size() -> usize { 8 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.x.to_le_bytes());
+        vec_data.extend_from_slice(&self.y.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- Attitude -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Attitude {
+    pub roll: i16,
+    pub pitch: i16,
+    pub yaw: i16,
+}
+
+
+impl Attitude {
+    pub fn new() -> Attitude{
+        Attitude {
+            roll: 0,
+            pitch: 0,
+            yaw: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Attitude, &'static str> {
+        if slice_data.len() == Attitude::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Attitude{
+                roll: ext.get_i16(),
+                pitch: ext.get_i16(),
+                yaw: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Attitude {
+    fn size() -> usize { 6 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.roll.to_le_bytes());
+        vec_data.extend_from_slice(&self.pitch.to_le_bytes());
+        vec_data.extend_from_slice(&self.yaw.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+
 // -- Motion -----------------------------------------------------------------------------------------------
 #[derive(Debug)]
 pub struct Motion {
@@ -76,3 +231,381 @@ impl Serializable for Motion {
         vec_data
     }
 }
+
+
+
+// -- Range -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Range {
+    pub left: i16,
+    pub front: i16,
+    pub right: i16,
+    pub rear: i16,
+    pub top: i16,
+    pub bottom: i16,
+}
+
+
+impl Range {
+    pub fn new() -> Range{
+        Range {
+            left: 0,
+            front: 0,
+            right: 0,
+            rear: 0,
+            top: 0,
+            bottom: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Range, &'static str> {
+        if slice_data.len() == Range::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Range{
+                left: ext.get_i16(),
+                front: ext.get_i16(),
+                right: ext.get_i16(),
+        
+                rear: ext.get_i16(),
+                top: ext.get_i16(),
+                bottom: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Range {
+    fn size() -> usize { 12 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.left.to_le_bytes());
+        vec_data.extend_from_slice(&self.front.to_le_bytes());
+        vec_data.extend_from_slice(&self.right.to_le_bytes());
+
+        vec_data.extend_from_slice(&self.rear.to_le_bytes());
+        vec_data.extend_from_slice(&self.top.to_le_bytes());
+        vec_data.extend_from_slice(&self.bottom.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- Position -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+
+impl Position {
+    pub fn new() -> Position{
+        Position {
+            x: 0.0_f32,
+            y: 0.0_f32,
+            z: 0.0_f32,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Position, &'static str> {
+        if slice_data.len() == Position::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Position{
+                x: ext.get_f32(),
+                y: ext.get_f32(),
+                z: ext.get_f32(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Position {
+    fn size() -> usize { 12 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.x.to_le_bytes());
+        vec_data.extend_from_slice(&self.y.to_le_bytes());
+        vec_data.extend_from_slice(&self.z.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- PositionVelocity -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct PositionVelocity {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub vx: f32,
+    pub vy: f32,
+    pub vz: f32,
+}
+
+
+impl PositionVelocity {
+    pub fn new() -> PositionVelocity{
+        PositionVelocity {
+            x: 0.0_f32,
+            y: 0.0_f32,
+            z: 0.0_f32,
+            vx: 0.0_f32,
+            vy: 0.0_f32,
+            vz: 0.0_f32,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<PositionVelocity, &'static str> {
+        if slice_data.len() == PositionVelocity::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(PositionVelocity{
+                x: ext.get_f32(),
+                y: ext.get_f32(),
+                z: ext.get_f32(),
+                vx: ext.get_f32(),
+                vy: ext.get_f32(),
+                vz: ext.get_f32(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for PositionVelocity {
+    fn size() -> usize { 24 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.x.to_le_bytes());
+        vec_data.extend_from_slice(&self.y.to_le_bytes());
+        vec_data.extend_from_slice(&self.z.to_le_bytes());
+        vec_data.extend_from_slice(&self.vx.to_le_bytes());
+        vec_data.extend_from_slice(&self.vy.to_le_bytes());
+        vec_data.extend_from_slice(&self.vz.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+
+// -- Bias -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Bias {
+    pub accel_x: i16,
+    pub accel_y: i16,
+    pub accel_z: i16,
+    pub gyro_roll: i16,
+    pub gyro_pitch: i16,
+    pub gyro_yaw: i16,
+}
+
+
+impl Bias {
+    pub fn new() -> Bias{
+        Bias {
+            accel_x: 0,
+            accel_y: 0,
+            accel_z: 0,
+            gyro_roll: 0,
+            gyro_pitch: 0,
+            gyro_yaw: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Bias, &'static str> {
+        if slice_data.len() == Bias::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Bias{
+                accel_x: ext.get_i16(),
+                accel_y: ext.get_i16(),
+                accel_z: ext.get_i16(),
+        
+                gyro_roll: ext.get_i16(),
+                gyro_pitch: ext.get_i16(),
+                gyro_yaw: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Bias {
+    fn size() -> usize { 12 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.accel_x.to_le_bytes());
+        vec_data.extend_from_slice(&self.accel_y.to_le_bytes());
+        vec_data.extend_from_slice(&self.accel_z.to_le_bytes());
+
+        vec_data.extend_from_slice(&self.gyro_roll.to_le_bytes());
+        vec_data.extend_from_slice(&self.gyro_pitch.to_le_bytes());
+        vec_data.extend_from_slice(&self.gyro_yaw.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+
+// -- Trim -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct Trim {
+    pub roll: i16,
+    pub pitch: i16,
+    pub yaw: i16,
+    pub throttle: i16,
+}
+
+
+impl Trim {
+    pub fn new() -> Trim{
+        Trim {
+            roll: 0,
+            pitch: 0,
+            yaw: 0,
+            throttle: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<Trim, &'static str> {
+        if slice_data.len() == Trim::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(Trim{
+                roll: ext.get_i16(),
+                pitch: ext.get_i16(),
+                yaw: ext.get_i16(),
+                throttle: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for Trim {
+    fn size() -> usize { 8 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.roll.to_le_bytes());
+        vec_data.extend_from_slice(&self.pitch.to_le_bytes());
+        vec_data.extend_from_slice(&self.yaw.to_le_bytes());
+        vec_data.extend_from_slice(&self.throttle.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+// -- LostConnection -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct LostConnection {
+    pub time_neutral: u16,
+    pub time_landing: u16,
+    pub time_stop: u32,
+}
+
+
+impl LostConnection {
+    pub fn new() -> LostConnection{
+        LostConnection {
+            time_neutral: 0,
+            time_landing: 0,
+            time_stop: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<LostConnection, &'static str> {
+        if slice_data.len() == LostConnection::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(LostConnection{
+                time_neutral: ext.get_u16(),
+                time_landing: ext.get_u16(),
+                time_stop: ext.get_u32(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for LostConnection {
+    fn size() -> usize { 8 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.time_neutral.to_le_bytes());
+        vec_data.extend_from_slice(&self.time_landing.to_le_bytes());
+        vec_data.extend_from_slice(&self.time_stop.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
+
+// -- MagnetometerOffset -----------------------------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct MagnetometerOffset {
+    pub offset: i16,
+}
+
+
+impl MagnetometerOffset {
+    pub fn new() -> MagnetometerOffset{
+        MagnetometerOffset {
+            offset: 0,
+        }
+    }
+    
+    pub fn parse(slice_data: &[u8]) -> Result<MagnetometerOffset, &'static str> {
+        if slice_data.len() == MagnetometerOffset::size() {
+            let mut ext: Extractor = Extractor::from_slice(slice_data);
+            Ok(MagnetometerOffset{
+                offset: ext.get_i16(),
+            })
+        }
+        else { Err("Wrong length") }
+    }
+}
+
+
+impl Serializable for MagnetometerOffset {
+    fn size() -> usize { 2 }
+
+
+    fn to_vec(&self) -> Vec<u8> {
+        let mut vec_data : Vec<u8> = Vec::new();
+
+        vec_data.extend_from_slice(&self.offset.to_le_bytes());
+
+        vec_data
+    }
+}
+
+
